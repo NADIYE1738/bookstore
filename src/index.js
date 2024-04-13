fda01106abd8f5d5165caf69bd61b0113ce7292d
NADIYE ENOSI <dericksenos72@gmail.com>
2024-04-13 23:11:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import React from "react"
-import { ReactDOM } from "react-dom/client";
+import React from "react";
+import "./index.css";
+import App from "./component/app";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>                  
+  <App />
+</BrowserRouter>,
+);
+
+
+reportWebVitals();
