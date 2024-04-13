f5d5e8009b9d84a78f10ba4e1e1c6084e0fdcc54
NADIYE ENOSI <dericksenos72@gmail.com>
2024-04-13 23:10:35 +0300

    Add db.js

---
+++ src/component/db.js
@@ -0,0 +1,116 @@
+const Books =  [
+      {
+        "id": "1",
+        "name": "Stocks",
+        "price": 5000,
+        "isInCart": false,
+        "wishlist": false,
+        "count": 0
+      },
+      {
+        "id": "2",
+        "name": "Economy",
+        "price": 5000,
+        "isInCart": false,
+        "wishlist": false,
+        "count": 0
+      },
+      {
+        "id": "3",
+        "name": "Politics",
+        "price": 2000,
+        "isInCart": false,
+        "wishlist": false,
+        "count": 0
+      },
+      {
+        "id": "4",
+        "name": "Stocks",
+        "price": 19000,
+        "isInCart": false,
+        "wishlist": false,
+        "count": 0
+      },
+      {
+        "id": "5",
+        "name": "Economy",
+        "price": 19000,
+        "isInCart": false,
+        "wishlist": false,
+        "count": 0
+      },
+      {
+        "id": "6",
+        "name": "Politics",
+        "price": 12000,
+        "isInCart": false,
+        "wishlist": false,
+        "count": 0
+      },
+      {
+        "id": "7",
+        "name": "Novel",
+        "price": 7000,
+        "isInCart": false,
+        "wishlist": false,
+        "count": 0
+      },
+      {
+        "id": "8",
+        "name": "Economy",
+        "price": 6000,
+        "count": 0,
+        "wishlist": false,
+        "isInCart": false
+      },
+      {
+        "id": "9",
+        "name": "Tech",
+        "price": 5000,
+        "count": 0,
+        "wishlist": false,
+        "isInCart": false
+      },
+      {
+        "id": "10",
+        "name": "Religion",
+        "price": 18000,
+        "count": 0,
+        "wishlist": false,
+        "isInCart": false
+      },
+      {
+        "id": "11",
+        "name": "Politics",
+        "price": 8000,
+        "wishlist": false,
+        "isInCart": false,
+        "count": 0
+      },
+      {
+        "id": "12",
+        "name": "Novel",
+        "price": 10000,
+        "isInCart": false,
+        "wishlist": false,
+        "count": 0
+      },
+      {
+        "id": "13",
+        "name": "Tech",
+        "price": 5000,
+        "count": 0,
+        "wishlist": false,
+        "isInCart": false
+      },
+      {
+        "id": "14",
+        "name": "Tech",
+        "price": 3000,
+        "count": 0,
+        "wishlist": false,
+        "isInCart": false
+      }
+    ];
+
+export default Books;
